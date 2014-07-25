a27f656ba20113d7c1c3c3ddba1163ace9a67ed2
Andy King <Andy King@localhost>
2014-07-24 23:56:07 -0700

    Continue development of the app tab panel widget.

--- src/org/fosterapet/client/FosterAPet.java
+++ src/org/fosterapet/client/FosterAPet.java
@@ -29,7 +29,7 @@ public void onModuleLoad() {
                           clientFactory.getRemoteService(), clientFactory.getValidators());
   clientFactory.setMainLayoutWidget(new MainLayoutWidget());
   final boolean loadTestData = false;
-  clientFactory.getMainLayoutWidget().createPetGrid(-1);
+  clientFactory.getMainLayoutWidget().getAppTabPanelWidget().createPetGrid();
   final GLGridWidget gridWidget = GridWidgetManager.getPetGrid("Pets1");
   if (loadTestData) {
     final GLListStore petTypeListStore = new GLListStore();
