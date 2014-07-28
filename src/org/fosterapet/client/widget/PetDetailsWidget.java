3567fa30bdc80a48071ac58be3b467564062e305
aww1122 <aww1122@localhost>
2014-07-28 11:47:42 -0700

    Begin work on the editor framework.

--- src/org/fosterapet/client/widget/PetDetailsWidget.java
+++ src/org/fosterapet/client/widget/PetDetailsWidget.java
@@ -1,5 +1,6 @@
 package org.fosterapet.client.widget;
 
+import org.greatlogic.glgwt.client.core.GLLog;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
@@ -7,11 +8,14 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.sencha.gxt.core.client.dom.ScrollSupport.ScrollMode;
 import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
+import com.sencha.gxt.widget.core.client.form.TextField;
 
 public class PetDetailsWidget extends Composite {
 //--------------------------------------------------------------------------------------------------
 @UiField
 FlowLayoutContainer flowLayoutContainer;
+@UiField
+TextField           petNameField;
 //==================================================================================================
 interface PetDetailsWidgetUiBinder extends UiBinder<Widget, PetDetailsWidget> { //
 }
@@ -20,6 +24,7 @@ public PetDetailsWidget() {
   final PetDetailsWidgetUiBinder uiBinder = GWT.create(PetDetailsWidgetUiBinder.class);
   initWidget(uiBinder.createAndBindUi(this));
   flowLayoutContainer.setScrollMode(ScrollMode.AUTO);
+  GLLog.popup(10, petNameField.getItemId());
 }
 //--------------------------------------------------------------------------------------------------
 }
---
+++ src/org/greatlogic/glgwt/client/editor/GLRecordEditorDriver.java
@@ -0,0 +1,96 @@
+package org.greatlogic.glgwt.client.editor;
+
+import java.util.List;
+import javax.validation.ConstraintViolation;
+import org.greatlogic.glgwt.client.core.GLRecord;
+import com.google.gwt.editor.client.EditorDriver;
+import com.google.gwt.editor.client.EditorError;
+import com.google.gwt.editor.client.EditorVisitor;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.container.Container;
+import com.sencha.gxt.widget.core.client.event.AddEvent;
+import com.sencha.gxt.widget.core.client.event.AddEvent.AddHandler;
+import com.sencha.gxt.widget.core.client.event.RemoveEvent;
+import com.sencha.gxt.widget.core.client.event.RemoveEvent.RemoveHandler;
+
+public class GLRecordEditorDriver implements EditorDriver<GLRecord> {
+//--------------------------------------------------------------------------------------------------
+@Override
+public void accept(final EditorVisitor visitor) {
+  // todo Auto-generated method stub
+}
+//--------------------------------------------------------------------------------------------------
+private void addChildWidgets(final Container container) {
+  for (final Widget childWidget : container) {
+    if (childWidget instanceof Container) {
+      addChildWidgets((Container)childWidget);
+    }
+    else {
+      addWidget(childWidget);
+    }
+  }
+}
+//--------------------------------------------------------------------------------------------------
+private void addWidget(final Widget widget) {
+  if (widget_represents_a_column()) {
+    add_widget();
+  }
+}
+//--------------------------------------------------------------------------------------------------
+public void edit(final GLRecord record) {
+
+}
+//--------------------------------------------------------------------------------------------------
+@Override
+public GLRecord flush() {
+  // todo Auto-generated method stub
+  return null;
+}
+//--------------------------------------------------------------------------------------------------
+@Override
+public List<EditorError> getErrors() {
+  // todo Auto-generated method stub
+  return null;
+}
+//--------------------------------------------------------------------------------------------------
+@Override
+public boolean hasErrors() {
+  // todo Auto-generated method stub
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
+public void initialize(final Container container) {
+  addChildWidgets(container);
+  container.addAddHandler(new AddHandler() {
+    @Override
+    public void onAdd(final AddEvent event) {
+      if (adding_a_container_widget()) {
+        addChildWidgets(new_container);
+      }
+      else {
+        addWidget(widget);
+      }
+    }
+  });
+  container.addRemoveHandler(new RemoveHandler() {
+    @Override
+    public void onRemove(final RemoveEvent event) {
+      // todo Auto-generated method stub
+
+    }
+  });
+}
+//--------------------------------------------------------------------------------------------------
+@Override
+public boolean isDirty() {
+  // todo Auto-generated method stub
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
+@Override
+public boolean setConstraintViolations(final Iterable<ConstraintViolation<?>> violations) {
+  // todo Auto-generated method stub
+  return false;
+}
+//--------------------------------------------------------------------------------------------------
+}
