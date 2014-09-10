97bf0d5b9444eb38edc24efc22d82bd67dd2205e
aww1122 <aww1122@localhost>
2014-09-10 14:08:03 -0700

    Change the way the configuration filename is determined.

--- src/org/fosterapet/server/FAPServletContextListener.java
+++ src/org/fosterapet/server/FAPServletContextListener.java
@@ -15,6 +15,7 @@ package org.fosterapet.server;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import org.apache.commons.lang3.StringUtils;
 import org.fosterapet.shared.IDBEnums.EFAPTable;
 import org.fosterapet.shared.IDBEnums.NextId;
 import org.greatlogic.glgwt.server.GLServerUtil;
@@ -38,7 +39,9 @@ public void contextDestroyed(final ServletContextEvent event) {
 @Override
 public void contextInitialized(final ServletContextEvent event) {
   final ServletContext servletContext = event.getServletContext();
-  final String configFilename = servletContext.getRealPath("cfg/FosterAPet.cfg").replace('\\', '/');
+  String configFilename = servletContext.getInitParameter("ConfigFilename");
+  configFilename = StringUtils.isBlank(configFilename) ? "cfg/FosterAPet.cfg" : configFilename;
+  configFilename = servletContext.getRealPath(configFilename).replace('\\', '/');
   GLUtil.initializeProgram(new FAPProgram(), null, null, true, //
                            "<args ConfigFilename='" + configFilename + "'/>");
   GLServerUtil.initialize(EFAPTable.NextId.name(), NextId.NextIdName.name(),
